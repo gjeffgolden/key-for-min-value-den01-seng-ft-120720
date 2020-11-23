c7bec1cd214eaa67b90509eb019a4fa401f2fee3
Jeff Golden <gjgolden08@gmail.com>
2020-11-23 21:31:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,11 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   minimum_value = 
-  maximum_value = name_hash.max
+  maximum_pair = name_hash.max 
+  maximum_value = maximum_pair[:key]
   name_hash.each do |key, value|
-    if value <  
+    
+    if value.is_max?
       minimum_value = value
     end
     if value < minimum_value
