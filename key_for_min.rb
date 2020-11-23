cb0dfd08cb2fb474a96ae4487123e9fc88368763
Jeff Golden <gjgolden08@gmail.com>
2020-11-23 23:04:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
-      key_for_min_value = key 
     end
   end
-  key_for_min_value
+  min_value
 end
