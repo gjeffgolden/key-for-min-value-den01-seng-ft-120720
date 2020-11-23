51a39a4667a25c80b7af5bf28fe09146ba373515
Jeff Golden <gjgolden08@gmail.com>
2020-11-23 23:08:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,10 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = 1000 
-  min_key = 
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
-      min_key = key 
+      puts key  
     end
   end
   min_key
