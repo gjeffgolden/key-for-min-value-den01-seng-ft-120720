19fed09a09eb4fff38c8dc0342d5739871b39d7c
Jeff Golden <gjgolden08@gmail.com>
2020-11-23 21:03:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  maximum_value = name_hash[:key].max 
   name_hash.each do |key, value|
     if value < maximum_value 
       minimum_value = value
